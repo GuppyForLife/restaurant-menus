e078a7da1331065484618d026ceed4868c7a98fe
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-06-30 13:32:29 -0700

    All tests work with models working

--- index.test.js
+++ index.test.js
@@ -15,26 +15,35 @@ describe('Restaurant and Menu Models', () => {
 
   test('can create a Restaurant', async () => {
     // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+    const newRest = await Restaurant.create(seedRestaurant[0]);
+    expect(newRest.name).toEqual(seedRestaurant[0].name);
   });
 
   test('can create a Menu', async () => {
     // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+    const newMenu = await Menu.create(seedMenu[0]);
+    expect(newMenu.title).toEqual(seedMenu[0].title);
   });
 
   test('can find Restaurants', async () => {
     // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+    const allRestaurants = await Restaurant.findAll();
+    expect(allRestaurants.length).toEqual(1);
+    expect(allRestaurants[0].name).toEqual(seedRestaurant[0].name)
   });
 
   test('can find Menus', async () => {
     // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+    const allMenus = await Menu.findAll();
+    expect(allMenus.length).toEqual(1);
+    expect(allMenus[0].title).toEqual(seedMenu[0].title)
   });
 
   test('can delete Restaurants', async () => {
     // TODO - write test
-    expect('NO TEST').toEqual('EXPECTED DATA');
+    const foundRest = await Restaurant.findAll();
+    const deletedRestaurant = await foundRest[0].destroy();
+    expect(Restaurant.length).toEqual(0);
+    expect(deletedRestaurant.name).toEqual(seedRestaurant[0].name)
   });
 });
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,7 +2,7 @@ const { sequelize } = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Restaurant model
-let Restaurant = sequelize.define(
+let Restaurant = sequelize.define('restaurant',
     {
         name: Sequelize.STRING,
         location: Sequelize.STRING,
