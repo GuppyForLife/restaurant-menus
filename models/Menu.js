0f39f5250588dc88e49965ff833f9160526486ad
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-06-30 13:15:56 -0700

    Models complete and dB connection established

--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,10 @@ const { sequelize } = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
+let Menu = sequelize.define('menu', 
+    {
+        title: Sequelize.STRING
+    }
+);
 
 module.exports = { Menu };
